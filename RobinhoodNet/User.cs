fecf7f20dce1b99823c84c336eda98f52ef910aa
bchantech <b_chan1@u.pacific.edu>
2016-07-23 22:56:00 -0700

    Added User classes

--- RobinhoodNet/User.cs
+++ RobinhoodNet/User.cs
@@ -29,5 +29,40 @@ namespace BasicallyMe.RobinhoodNet
     // TODO: Implement me!
 	public class User
 	{
-	}
+        public string Username { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        Url<UserIdInfo> IdInfo { get; set; }
+        Url<User> UserURL { get; set; }
+        DateTime CreatedAt { get; set; }
+        Url<UserBasicInfo> BasicInfo { get; set; }
+        string email { get; set; }
+        Url<UserInvestmentProfile> InvestmentProfile { get; set; }
+        string id { get; set; }
+
+        // returns not found
+        //Url<UserInternationalInfo> InternationalInfo { get; set; }
+
+        Url<UserEmployment> Employment { get; set; }
+        Url<UserAdditionalInfo> AdditionalInfo { get; set; }
+        
+        public User() { }
+
+        internal User(Newtonsoft.Json.Linq.JToken json) : this()
+        {
+
+            Username = (string)json["username"];
+            FirstName = (string)json["first_name"];
+            LastName = (string)json["last_name"];
+            IdInfo = new Url<UserIdInfo>((string)json["id_info"]);
+            UserURL = new Url<User>((string)json["user"]);
+            CreatedAt = (DateTime)json["created_at"];
+            BasicInfo = new Url<UserBasicInfo>((string)json["basic_info"]);
+            email = (string)json["email"];
+            InvestmentProfile = new Url<UserInvestmentProfile>((string)json["investment_profile"]);
+            id = (string)json["id"];
+            Employment = new Url<UserEmployment>((string)json["employment"]);
+            AdditionalInfo = new Url<UserAdditionalInfo>((string)json["additional_info"]);
+        }
+}
 }
