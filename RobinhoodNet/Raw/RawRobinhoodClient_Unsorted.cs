79b013b2e398abe041f89d721ff15568f4639076
bchantech <b_chan1@u.pacific.edu>
2016-07-29 17:05:42 -0700

    Add classes to project file
    Updated Project file, added Raw Client Unsorted to include other class
    functions that doesn't have their own file

---
+++ RobinhoodNet/Raw/RawRobinhoodClient_Unsorted.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace BasicallyMe.RobinhoodNet.Raw
+{
+    public partial class RawRobinhoodClient
+    {
+        
+    }
+}
